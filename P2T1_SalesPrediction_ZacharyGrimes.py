b1c9ba4f6e431c8ad2e8b76f9df4b4647f464ca2
Panthersfan2020 <43148045+Panthersfan2020@users.noreply.github.com>
2018-09-10 13:54:59 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_ZacharyGrimes.py
@@ -0,0 +1,23 @@
+#This is my second python program. It might not be good. but im trying.
+#Wednesday September 5, 2018
+#CTI-110 P2T1- Sales Prediction
+#Zachary Grimes
+#
+#Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+ RESTART: C:/Users/grimesz1845/Documents/P2T1_SalesPrediction_ZacharyGrimes.py 
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
+
